55fbf18fbce20d03fe212661c5a2a2664117fcb6
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2022-09-12 22:00:07 +0530

    javascript done

---
+++ app.js
@@ -0,0 +1,38 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-btn");
+const message = document.querySelector("#error-message");
+const noOfNotes=document.querySelectorAll(".no-of-notes")
+
+const availableNotes=[2000,500,100,20,10,5,1];
+checkButton.addEventListener("click", function validateBillAndCashAmt() {
+  hideMessage();
+  if (billAmount.value > 0) {
+    if (billAmount.value >= cashGiven.value) {
+       const amountToBeReturned=cashGiven.value-billAmount.value ;
+       calculateChange(amountToBeReturned);
+    } else {
+      showMessage("The given cash should at least equal to the amount of bill. ")
+    }
+
+  } else {
+    showMessage("Invalied bill amount");
+  }
+
+});
+
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = msg;
+}
+
+function hideMessage() {
+  message.style.display = "none";
+}
+function calculateChange(amountToBeReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    var numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
+    amountToBeReturned%=availableNotes[i];
+    noOfNotes[i].innerText=numberOfNotes;
+  }
+}
